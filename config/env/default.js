c93bdf3cfed1829f97e3fa8c6b19ec181e14892f
Fernando V <fvalarinon@gmail.com>
2017-04-21 14:21:08 +0200

    removed unused default files.
    Signed-off-by: Fernando V <fvalarinon@gmail.com>

--- config/env/default.js
+++ config/env/default.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
+    title: 'Challenge App',
     description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
@@ -49,7 +49,7 @@ module.exports = {
     profileUpload: {
       dest: './modules/users/client/img/profile/uploads/', // Profile upload destination path
       limits: {
-        fileSize: 1 * 1024 * 1024 // Max file size in bytes (1 MB)
+        fileSize: 10 1024 * 1024 // Max file size in bytes (10 MB)
       }
     }
   },
